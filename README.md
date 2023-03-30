b54f28ad12fd7975429f978e3d8618ccab6aa430
Frantisek Brabec <brabec@gmail.com>
2023-03-30 12:02:02 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# stopwatch
+ Two ESP32 boards, one with an OLED display, each with a button measure time between two button presses
---
+++ stopwatch.ino
@@ -0,0 +1,175 @@
+#define STOPWATCH_START 0
+
+#define BUTTON_PIN 32 // GIOP21 pin connected to button
+int lastState = HIGH; // the previous state from the input pin
+int currentState;     // the current reading from the input pin
+
+#include <esp_now.h>
+#include <WiFi.h>
+#include <Wire.h>
+
+#if !(STOPWATCH_START)
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
+
+int lastDisplay = millis();
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+#endif
+
+
+// REPLACE WITH THE MAC Address of your receiver 
+#if !(STOPWATCH_START)
+uint8_t broadcastAddress[] = {0x24, 0x6F, 0x28, 0xA4, 0x83, 0x0C};
+#else
+uint8_t broadcastAddress[] = {0x24, 0x6F, 0x28, 0x1F, 0x3C, 0x70};
+#endif
+
+// Variable to store if sending data was successful
+String success;
+
+#define INCOMING_LENGTH 10
+char incomingDummy[INCOMING_LENGTH];
+
+esp_now_peer_info_t peerInfo;
+
+#if STOPWATCH_START
+
+// Callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  if (status ==0){
+    success = "Delivery Success :)";
+  } else{
+    success = "Delivery Fail :(";
+  }
+}
+
+#else
+int startTime = 0, stopTime = 0;
+boolean running = false;  
+
+// Callback when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t* incoming, int len) {
+  memcpy(incomingDummy, incoming, INCOMING_LENGTH);
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+
+  // reset and start clock
+  startTime = millis();
+  running = true;
+}
+
+#endif
+ 
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(115200);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+#if !(STOPWATCH_START)
+  // Init OLED display
+  startTime = millis();
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+#endif
+
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+#if STOPWATCH_START
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+#endif
+  
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+
+#if !(STOPWATCH_START)
+  // Register for a callback function that will be called when data is received
+  esp_now_register_recv_cb(OnDataRecv);
+#endif
+
+}
+ 
+void loop() {
+  currentState = digitalRead(BUTTON_PIN);
+
+  if(lastState == LOW && currentState == HIGH) {
+    // button pressed
+#if STOPWATCH_START
+    // Send message via ESP-NOW
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)incomingDummy, INCOMING_LENGTH);
+     
+    if (result == ESP_OK) {
+      Serial.println("Sent with success");
+    }
+    else {
+      Serial.println("Error sending the data");
+    }
+#else    
+    stopTime = millis();
+    running = false;
+#endif
+  
+  }
+
+  lastState = currentState;
+
+#if !(STOPWATCH_START)
+  if (millis() - lastDisplay > 1000) {
+    lastDisplay = millis();
+    updateDisplay();
+  }
+#endif
+}
+
+#if !(STOPWATCH_START)
+void updateDisplay(){
+  int elapsed;
+  if (running) {
+    elapsed = (millis() - startTime);
+  } else {
+    elapsed = (stopTime - startTime);
+  }
+
+  // Display Readings on OLED Display
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println("Elapsed Time");
+  display.setCursor(0, 15);
+  display.setTextSize(5);
+  display.print(elapsed / 1000);
+  display.display();
+  
+  // Display Readings in Serial Monitor
+  Serial.print("Clock: ");
+  Serial.println(elapsed / 1000);
+  Serial.print("Running: ");
+  Serial.println(running ? "true" : "false");
+}
+#endif
